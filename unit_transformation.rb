23a26de0017e62d1ac3beebe006d4cda432f589b
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-05-28 22:36:22 -0300

    validate

--- unit_transformation.rb
+++ unit_transformation.rb
@@ -1,6 +1,10 @@
 class UnitTransformation < UnitTool
 
   def process_unit
+    (@unit_type.transformation.nil?) ? nil : transformation
+  end
+
+  def transformation
     if @manager.available?(@unit_type.transformation_cost)
       @unit.unit_transformation(@unit_type.transformation)
       @manager.subtract(@unit_type.transformation_cost)
